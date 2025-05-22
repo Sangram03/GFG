a1b48b515312e7dfbe8a3871277a80c21aead756
Sangram Das <121278559+Sangram03@users.noreply.github.com>
2025-05-23 00:21:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int noOfWays(int m, int n, int x) {
+        int[][] dp = new int[n + 1][x + 1];
+        
+        // Base case: One way to reach sum 0 with 0 dice
+        dp[0][0] = 1;
+
+        for (int dice = 1; dice <= n; dice++) {
+            for (int sum = 1; sum <= x; sum++) {
+                for (int face = 1; face <= m; face++) {
+                    if (sum - face >= 0) {
+                        dp[dice][sum] += dp[dice - 1][sum - face];
+                    }
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+}
